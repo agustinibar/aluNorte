2cce89df6f84f8fb7b5415c93ac1442c4c6a7d9f
agustinibar <agustinibarperrotta@gmail.com>
2024-05-08 15:22:13 -0300

    first commit

--- src/view/Landing/Landing.jsx
+++ src/view/Landing/Landing.jsx
@@ -1,8 +1,14 @@
 
 import styles from './landing.module.css';
 import backgroundVideo from '../../assets/background.mp4';
+import { useNavigate } from 'react-router-dom';
 
 const Landing = () => {
+  const navigate = useNavigate();
+
+  const handleClickIngresar = ()=>{
+    navigate('/home')
+  }
   return (
     <div className={styles.landingContainer}>
       <video className={styles.video} autoPlay loop muted>
@@ -11,7 +17,8 @@ const Landing = () => {
       <header className={styles.header}>
         <div className={styles.textContainer}>
           <h1>Bienvenido a AluNorte</h1>
-          <p>aberturas</p>
+          <p><button className={styles.ingresar} onClick={handleClickIngresar}>Ingresar</button></p>
+          
         </div>
       </header>
     </div>
